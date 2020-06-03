9a67e837219141f3995663817db10d3c2c3d69ea
Thomas <th.mendes1997@gmail.com>
2020-06-03 17:28:56 -0300

    New Project, Example Model, Example Controller

---
+++ WebApi/Controllers/ExampleController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("Example")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+        
+    }
+}
---
+++ WebApi/Models/Example.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApi.Models
+{
+    public class Example
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int Quantity { get; set; }
+        public bool Available { get; set; }
+    }
+}
