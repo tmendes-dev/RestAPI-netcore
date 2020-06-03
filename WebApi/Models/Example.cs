2e2932cfcb76a0659b6d549e500015b152444384
Thomas <th.mendes1997@gmail.com>
2020-06-03 20:36:37 -0300

    Model Data Anonotations

--- WebApi/Models/Example.cs
+++ WebApi/Models/Example.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApi.Models
 {
     public class Example
     {
+        [Key]
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
+
+        [Required]
         public int Quantity { get; set; }
+
+        [Required]
         public bool Available { get; set; }
     }
 }
